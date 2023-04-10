98f4fe5894aa5628f3e6c7b0037b21ede35654dd
2DC <kkts9308@gmail.com>
2023-04-10 23:38:06 +0900

    헤더 반응형 수정

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,21 @@
 <template>
   <header class="font-old">
-    <div class="container-header">
-      <h1 class="h1-header">&lt; 2DC JS /&gt;</h1>
-      <ul class="ul-header">
+    <nav class="navbar">
+      <h1 class="logo-header">&lt; 2DC JS /&gt;</h1>
+      <ul class="navbar-menu">
         <li>CURRICULUM</li>
         <li>GITHUB</li>
         <li>COMMENT</li>
         <li>Not Use Yet</li>
       </ul>
-      <div class="container-login">
+      <div class="login-container">
         <button>Login</button>
         <button>SignUp</button>
       </div>
-      <button class="menu-header"><i class="fa-solid fa-bars" style="color: #000000"></i></button>
-    </div>
+      <button class="navbar-togglebtn">
+        <i class="fa-solid fa-bars" style="color: #000000"></i>
+      </button>
+    </nav>
   </header>
 </template>
 
@@ -22,74 +24,94 @@ import router from '@/router'
 </script>
 
 <style scoped>
-header {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  height: 65px;
-  font-size: 1.1rem;
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-}
-
-.container-header {
-  width: calc(100% - 50px);
+.navbar {
+  margin: 0 auto;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  padding: 15px 24px;
 }
 
-.container-login {
-  width: 200px;
+.logo-header {
+  font-size: 25px;
+}
+
+.login-container {
   display: flex;
-  justify-content: flex-start;
+  justify-content: space-between;
 }
 
-.container-login button {
+.login-container button {
   padding: 6px 13px;
   color: var(--vt-c-white-soft);
   background-color: var(--vt-c-black-soft);
 }
 
-.container-login button:first-child {
+.login-container button:first-child {
   margin-right: 25px;
   background-color: var(--header-bg-color);
   color: var(--vt-c-black-soft);
 }
 
-.h1-header {
-  font-size: 25px;
-}
-
-.ul-header {
+.navbar-menu {
   width: 500px;
   display: flex;
   justify-content: space-around;
 }
 
-li {
+.navbar-menu li {
   padding: 6px 13px;
   border-radius: 8px;
   background-color: var(--header-bg-color);
 }
 
-.menu-header {
-  position: absolute;
-  right: calc(25px);
-}
-
-@media screen and (min-width: 1050px) {
-  .menu-header {
-    display: none;
-  }
+.navbar-togglebtn {
+  display: none;
 }
 
 @media screen and (max-width: 1050px) {
-  .ul-header {
+  .navbar {
     flex-direction: column;
+    align-items: flex-start;
   }
-  .ul-header,
-  .container-login {
+
+  .navbar-menu {
+    height: 0px;
+    display: none;
+  }
+
+  .navbar-menu.active {
+    height: 100%;
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+  }
+
+  .navbar-menu li {
+    width: 50%;
+    margin: 5px;
+    text-align: center;
+  }
+
+  .login-container {
+    display: none;
+  }
+
+  .login-container.active {
+    width: 100%;
+    margin-top: 15px;
+    justify-content: center;
+    display: flex;
+  }
+
+  .navbar-togglebtn {
+    position: absolute;
+    display: block;
+    right: 25px;
+    top: 25px;
   }
 }
 </style>
