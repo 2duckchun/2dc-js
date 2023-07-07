3ae7d27b1c35b038dbf8cd98784db5f87bfc10a9
2DC <kkts9308@gmail.com>
2023-07-07 15:41:50 +0900

    refactor : fonts export명 수정

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,8 +1,8 @@
 import { M_PLUS_1 } from "next/font/google";
 
-const m_PLUS_1 = M_PLUS_1({
+const font_M_PLUS_1 = M_PLUS_1({
   subsets: ["latin"],
   variable: "--logo-font",
 });
 
-export { m_PLUS_1 };
+export { font_M_PLUS_1 };
