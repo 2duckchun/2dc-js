e57e1307114d3157fc4527702d173b7501eb5c88
2DC <kkts9308@gmail.com>
2023-07-08 16:17:04 +0900

    feat : Header에서  NavMenu 컴포넌트 분리

---
+++ src/app/(rootLayout)/_component/NavMenu.tsx
@@ -0,0 +1,12 @@
+import style from "./NavMenu.module.scss";
+
+export default function NavMenu() {
+  return (
+    <ul className={style.nav_menu}>
+      <li>자바스크립트</li>
+      <li>자료구조</li>
+      <li>REPO</li>
+      <li>코멘트</li>
+    </ul>
+  );
+}
