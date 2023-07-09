f1b9014cfd3454e385a547d14c1bd38fb6a5be17
2DC <kkts9308@gmail.com>
2023-07-09 20:56:13 +0900

    feat : 네비게이션 메뉴 컴포넌트 분리

--- src/app/(rootLayout)/_component/NavMenu.tsx
+++ src/app/(rootLayout)/_component/NavMenu.tsx
@@ -2,7 +2,7 @@ import style from "./NavMenu.module.scss";
 
 export default function NavMenu() {
   return (
-    <ul className={style.nav_menu}>
+    <ul className={`${style.nav_menu} nev_menu`}>
       <li>자바스크립트</li>
       <li>자료구조</li>
       <li>REPO</li>
