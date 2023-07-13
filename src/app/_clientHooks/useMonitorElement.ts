24d13dc2ff6071c71ed781c7efed34cb48675b53
2DC <kkts9308@gmail.com>
2023-07-13 21:28:28 +0900

    feat : useMonitorElement 훅 제작

---
+++ src/app/_clientHooks/useMonitorElement.ts
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from "react";
+
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+
+export default function useMonitorElement(callback: () => void) {
+  const monitoredElement = useRef(null);
+  useEffect(() => {
+    if (!monitoredElement.current) return;
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          observer.unobserve(entry.target);
+          callback();
+        }
+      });
+    }, options);
+    observer.observe(monitoredElement.current);
+
+    return () => observer.disconnect();
+  }, [callback, monitoredElement.current]);
+
+  return monitoredElement;
+}
