e7c605ef0d4f47821a30331f21875598021a9f1e
2DC <kkts9308@gmail.com>
2023-07-14 11:21:19 +0900

    useResponsiveHeader 내부에서 Ref를 직접 선언하여 return하도록 수정

--- src/app/_clientHooks/useResponsiveHeader.ts
+++ src/app/_clientHooks/useResponsiveHeader.ts
@@ -1,27 +1,29 @@
 import { MutableRefObject, useEffect, useRef } from "react";
 
 export default function useResponsiveHeader(
-  navbarRef: MutableRefObject<HTMLElement | null>,
-  collapseRef: MutableRefObject<HTMLDivElement | null>
+  headerCollapingHandler: (flag: Event | boolean) => void
 ) {
   const prevScrollY = useRef(0);
+  const navBarRef: MutableRefObject<HTMLElement | null> = useRef(null);
+  const collapseRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
+
   useEffect(() => {
     const stickyHeader = () => {
       let currentScrollY = window.scrollY;
-      if (navbarRef.current && collapseRef.current) {
-        if (prevScrollY.current > currentScrollY) {
-          navbarRef.current.style.top = "0";
-        } else {
-          navbarRef.current.style.top = "-60px";
-          collapseRef.current.classList.remove("active");
-        }
+      if (!navBarRef.current || !collapseRef.current) return;
+
+      if (prevScrollY.current > currentScrollY) {
+        navBarRef.current.style.top = "0";
+      } else {
+        navBarRef.current.style.top = "-60px";
+        headerCollapingHandler(false);
       }
       prevScrollY.current = currentScrollY;
     };
     window.addEventListener("scroll", stickyHeader);
 
-    return () => {
-      window.removeEventListener("scroll", stickyHeader);
-    };
+    return () => window.removeEventListener("scroll", stickyHeader);
   }, []);
+
+  return [navBarRef, collapseRef];
 }
