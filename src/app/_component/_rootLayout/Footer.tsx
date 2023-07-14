b60fd3f4c09e802403b2bac8bed0638b25452716
2DC <kkts9308@gmail.com>
2023-07-14 11:11:04 +0900

    feat : Footer 컴포넌트 개발

---
+++ src/app/_component/_rootLayout/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.scss";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>©Copyright 2023 2duckchun</p>
+    </footer>
+  );
+}
