a9d8c95e3b223ca499054be5280d50daa67e1c16
2DC <kkts9308@gmail.com>
2023-07-14 11:23:23 +0900

    refact : 경로 수정 및 useResponsiveHeader 훅이 수정된 것에 맞게 Header에 반영

--- src/app/_component/_rootLayout/Header.tsx
+++ src/app/_component/_rootLayout/Header.tsx
@@ -1,28 +1,30 @@
 "use client";
 
-import setThemeCookie from "../_utils/setThemeInCookie";
-import style from "./Header.module.scss";
-import IconMoon from "../../../public/icon-moon.svg";
-import IconSun from "../../../public/icon-sun.svg";
-import { useRef, useState } from "react";
-import cookieParser from "../_utils/cookieParser";
-import NavMenu from "./_component/NavMenu";
+import { useState } from "react";
 import { font_pretended } from "@/fonts/fonts";
-import NavToggleButton from "./_component/NavToggleButton";
-import useResponsiveHeader from "../_clientHooks/useResponsiveHeader";
+import setThemeCookie from "@/app/_utils/setThemeInCookie";
+import style from "./Header.module.scss";
+import IconMoon from "#/icon-moon.svg";
+import IconSun from "#/icon-sun.svg";
+import cookieParser from "@/app/_utils/cookieParser";
+import NavMenu from "./NavMenu";
+import NavToggleButton from "./NavToggleButton";
+import useResponsiveHeader from "@/app/_clientHooks/useResponsiveHeader";
 
 export default function Header({ currentTheme }: { currentTheme: string }) {
   const [theme, setTheme] = useState(currentTheme);
+  const [isCollapsed, setIsCollapsed] = useState(false);
   const ThemeIcon = theme === "dark" ? IconMoon : IconSun;
-  const navbarRef = useRef<HTMLElement | null>(null);
-  const collapseRef = useRef<HTMLDivElement | null>(null);
-  useResponsiveHeader(navbarRef, collapseRef);
 
-  const navToggleHandler = () => {
-    if (collapseRef.current) {
-      collapseRef.current.classList.toggle("active");
+  const headerCollapingHandler = (flag: Event | boolean) => {
+    if (!flag) {
+      setIsCollapsed(false);
+      return;
     }
+    setIsCollapsed(!isCollapsed);
   };
+
+  const [navBarRef, collapseRef] = useResponsiveHeader(headerCollapingHandler);
 
   const changeTheme = () => {
     const currentTheme = cookieParser("theme");
@@ -32,11 +34,13 @@ export default function Header({ currentTheme }: { currentTheme: string }) {
   };
 
   return (
-    <header className={style.header} ref={navbarRef}>
+    <header className={style.header} ref={navBarRef}>
       <div className={`${style.header_content} header_content`}>
         <p className={style.logo_text}>2DC</p>
         <nav
-          className={`${style.nav_collapse} ${font_pretended.className} nav_collapse`}
+          className={`${style.nav_collapse} ${font_pretended.className} ${
+            isCollapsed ? style.active : ""
+          }`}
           ref={collapseRef}
         >
           <NavMenu />
@@ -53,7 +57,7 @@ export default function Header({ currentTheme }: { currentTheme: string }) {
           </div>
         </nav>
       </div>
-      <NavToggleButton navToggleHandler={navToggleHandler} />
+      <NavToggleButton navToggleHandler={headerCollapingHandler} />
     </header>
   );
 }
