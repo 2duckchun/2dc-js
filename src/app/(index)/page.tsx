f7d56655cb450fa2188fb1b8cc1f8574aa0daeb3
2DC <kkts9308@gmail.com>
2023-07-14 11:36:31 +0900

    feat : 인덱스 페이지 컴포넌트에 웰컴 메세지, 웰컴 카드 컴포넌트 추가

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,9 +1,27 @@
+"use client";
+
 import styles from "./page.module.scss";
+import { useState } from "react";
+import useMonitorElement from "../_clientHooks/useMonitorElement";
+import WelcomeCard from "../_component/_index/WelcomeCard";
 
 export default function Home() {
+  const [isShowCard, setIsShowCard] = useState(false);
+  const showCardCallback = () => {
+    setIsShowCard(true);
+  };
+  const monitoredRef = useMonitorElement(showCardCallback);
+
   return (
-    <main className={styles.test_tomato}>
-      <p>도로로록</p>
-    </main>
+    <>
+      <div className={styles.welcome_message}>
+        <p ref={monitoredRef}>
+          이 곳은 <br />
+          제가 배운 것들을 공유하는 <span>공간</span>입니다.
+        </p>
+        <p>효과적으로 자바스크립트를 배울 수 있는 방법을 연구합니다.</p>
+      </div>
+      <WelcomeCard isShowCard={isShowCard} />
+    </>
   );
 }
