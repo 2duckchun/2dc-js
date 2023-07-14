75eb82f9c228aafe28f1f6098d707cd27a8f336d
2DC <kkts9308@gmail.com>
2023-07-14 11:09:25 +0900

    style : NavToggleButton 전역스타일 제거 및 모듈 CSS에 반응형 개별적으로 추가

--- src/app/_component/_rootLayout/NavToggleButton.tsx
+++ src/app/_component/_rootLayout/NavToggleButton.tsx
@@ -2,10 +2,7 @@ import style from "./NavToggleButton.module.scss";
 
 export default function NavToggleButton({ navToggleHandler }: any) {
   return (
-    <button
-      className={`${style.header_toggle} header_toggle`}
-      onClick={navToggleHandler}
-    >
+    <button className={style.header_toggle} onClick={navToggleHandler}>
       <div className={style.toggle_div1}></div>
       <div className={style.toggle_div2}></div>
       <div className={style.toggle_div3}></div>
