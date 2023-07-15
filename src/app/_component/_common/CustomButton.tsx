f996b8f342c3f5e6c7ed89d11f10dbe92f3a34b6
2DC <kkts9308@gmail.com>
2023-07-15 14:07:59 +0900

    feat : CustomButton 컴포넌트 제작(스타일을 상속받을 수 있게 설계함)

---
+++ src/app/_component/_common/CustomButton.tsx
@@ -0,0 +1,15 @@
+export default function CustomButton({
+  propStyle,
+  onClickHandler,
+  children,
+}: {
+  propStyle: string;
+  onClickHandler: () => void;
+  children: React.ReactNode;
+}) {
+  return (
+    <button className={propStyle} onClick={onClickHandler}>
+      {children}
+    </button>
+  );
+}
