3b2a4890d6d6b10f35187f022fb921100327dd37
2DC <kkts9308@gmail.com>
2023-07-15 14:15:14 +0900

    refact : NavMenu 폴더 변경(_component/_rootLayout으로 이동) 및 커리큘럽 접속 가능하도록 커스텀링크 추가

---
+++ src/app/_component/_rootLayout/NavMenu.tsx
@@ -0,0 +1,17 @@
+import CustomLink from "../_common/CustomLink";
+import styles from "./NavMenu.module.scss";
+
+export default function NavMenu() {
+  return (
+    <ul className={styles.nav_menu}>
+      <CustomLink href={{ pathname: "/curriculum/js" }}>
+        <li>자바스크립트</li>
+      </CustomLink>
+      <CustomLink href={{ pathname: "/curriculum/data-structure" }}>
+        <li>자료구조</li>
+      </CustomLink>
+      <li>REPO</li>
+      <li>코멘트</li>
+    </ul>
+  );
+}
