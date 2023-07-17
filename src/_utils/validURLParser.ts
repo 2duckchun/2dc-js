ffefa8d85d260d9a98ff10ca1019203da63eaa9e
2DC <kkts9308@gmail.com>
2023-07-17 10:44:30 +0900

    feat : validURLParser util 함수 제작(올바른 URL인지 파악하여 middleware에 알려줌)

---
+++ src/_utils/validURLParser.ts
@@ -0,0 +1,20 @@
+export default function validURLParser(
+  pathArray: string[],
+  jsCurriculumRegex: RegExp,
+  dsCurriculumRegex: RegExp
+) {
+  let isValidURL = true;
+
+  if (pathArray.length >= 2) {
+    const curriculumName = pathArray[0];
+    const curriculumPath = pathArray[1];
+
+    if (curriculumName === "js") {
+      isValidURL = jsCurriculumRegex.test(curriculumPath);
+    } else if (curriculumName === "data-structure") {
+      isValidURL = dsCurriculumRegex.test(curriculumPath);
+    }
+  }
+
+  return isValidURL;
+}
