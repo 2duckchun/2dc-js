c0c22ee848493822db76fa7b9fd2bccd5223fd68
2DC <kkts9308@gmail.com>
2023-07-17 14:15:41 +0900

    feat : 커리큘럼 카드 컴포넌트 작성

---
+++ src/app/_component/_curriculum/CurriculumCard.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { CurriculumCartPropsType } from "@/types/common";
+import styles from "./CurriculumCard.module.scss";
+import Image from "next/image";
+import CustomLink from "../_common/CustomLink";
+
+export default function CurriculumCard({
+  imageFile,
+  chapter,
+  subject,
+  explain,
+  curriculum,
+}: CurriculumCartPropsType) {
+  return (
+    <div className={styles.card_container}>
+      <div className={styles.image_container}>
+        <Image
+          className={styles.next_card_image}
+          src={imageFile}
+          alt={`${chapter}의 타이틀 명화`}
+        />
+      </div>
+      <div className={styles.card_info}>
+        <p className={styles.card_chapter}>{chapter}</p>
+        <h3 className={styles.card_title}>{subject}</h3>
+        <p>{explain}</p>
+        <div className={styles.card_button_container}>
+          <CustomLink
+            href={{
+              pathname: `/curriculum/${curriculum}/${chapter}`,
+            }}
+          >
+            <button className={styles.card_button}>공부하러 가기!</button>
+          </CustomLink>
+        </div>
+      </div>
+    </div>
+  );
+}
