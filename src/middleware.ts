ca53cfc94ec6fcc8b544b0417aeee67053389fdb
2DC <kkts9308@gmail.com>
2023-07-17 10:45:27 +0900

    feat : 커리큘럼의 경로를 추적할 middleware를 app router에 장착

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import validURLParser from "./_utils/validURLParser";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const pathArray = request.nextUrl.pathname.split("/").slice(2);
+  const isValidURL = validURLParser(
+    pathArray,
+    /^chapter[0-9]$/,
+    /^chapter[0-9]$/
+  );
+
+  if (!isValidURL) {
+    const url = request.nextUrl;
+    url.pathname = `/404`;
+    return NextResponse.rewrite(url);
+  }
+}
+
+export const config = {
+  matcher: ["/curriculum/js/:path*", "/curriculum/data-structure/:path*"],
+};
