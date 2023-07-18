7d7e13112df05a1045bf1fe9b56e15a153edd3c4
2DC <kkts9308@gmail.com>
2023-07-18 11:19:54 +0900

    refact - CodeBox에 className을 추가해서 Hydration 오류 수정

--- src/app/_component/_curriculum/CodeBox.tsx
+++ src/app/_component/_curriculum/CodeBox.tsx
@@ -1,20 +1,23 @@
-"use client"
-import { useEffect } from 'react'
-import Prism from "prismjs"
-import "prismjs/themes/prism-tomorrow.css"
+"use client";
+import { useEffect } from "react";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
 
-export default function CodeBox({code, language}: {
-    code: string,
-    language: string
+export default function CodeBox({
+  code,
+  language = "javascript",
+}: {
+  code: string;
+  language?: string;
 }) {
-    useEffect(() => {
-        Prism.highlightAll()
-    }, [])
-    return (
-        <div className='code'>
-            <pre>
-                <code className={`language-${language}`}>{code}</code>
-            </pre>
-        </div>
-    )
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+  return (
+    <div className="code">
+      <pre className={`language-${language}`} tabIndex={1}>
+        <code className={`language-${language}`}>{code}</code>
+      </pre>
+    </div>
+  );
 }
