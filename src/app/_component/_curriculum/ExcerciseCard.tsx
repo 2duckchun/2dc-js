1ec95c6a697b03d3293d27bff815e6d72d2bc248
2DC <kkts9308@gmail.com>
2023-07-24 22:49:29 +0900

    feat : ExerciseCard 컴포넌트 제작

---
+++ src/app/_component/_curriculum/ExcerciseCard.tsx
@@ -0,0 +1,24 @@
+import CodeBox from "./CodeBox";
+
+type ExerciseCardProps = {
+  title: string;
+  QuestionCode: string;
+  AnswerCode: string;
+};
+
+export default function ExerciseCard({
+  title,
+  QuestionCode,
+  AnswerCode,
+}: ExerciseCardProps) {
+  return (
+    <li>
+      <h4>{title}</h4>
+      <CodeBox code={QuestionCode} />
+      <details>
+        <summary>해설</summary>
+        <CodeBox code={AnswerCode} />
+      </details>
+    </li>
+  );
+}
