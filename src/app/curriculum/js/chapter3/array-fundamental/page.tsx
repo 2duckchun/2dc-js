c9bf515ace7421eff8d610b99ce94123f0da04ed
2DC <kkts9308@gmail.com>
2023-08-20 20:08:41 +0900

    배열 기초 교안 작성 완료

--- src/app/curriculum/js/chapter3/array-fundamental/page.tsx
+++ src/app/curriculum/js/chapter3/array-fundamental/page.tsx
@@ -24,6 +24,18 @@ a = 30`,
 let b = a
 a = 30
 b = a + b`,
+  primitiveCondition: `'2DC' === '2DC' // true
+5 === 5 // true
+undefined === undefined // true`,
+  referenceCondition: `[] === [] // false
+
+const obj1 = {}
+const obj2 = {}
+obj1 === obj2 // false
+
+const fn1 = () => {}
+const fn2 = () => {}
+fn1 === fn2 // false`,
   primitiveAndReference: `let a = 5
 let b = 5
 if (a === b) {
@@ -85,13 +97,58 @@ let colors = ['red', 'green', 'blue']
 function greet(name) {
   return 'hello' + name
 }`,
+  arrayExplain1: `const 과일바구니 = ['사과', '수박', '참외', '감']`,
+  arrayExplain2: `['사과' // 엘리먼트
+'수박', // 엘리먼트
+'참외', // 엘리먼트
+'감'// 엘리먼트 ]`,
+  arrayExplain3: `const 과일바구니 = ['사과', '수박', '참외', '감']
+과일바구니[0] // '사과'
+과일바구니[1] // '수박'
+과일바구니[2] // '참외'
+과일바구니[3] // '감'`,
+  arrayExplain4: `const 과일바구니 = ['사과', '수박', '참외', '감', 2, 100]
+let number = 0
+과일바구니[number] // '사과'
+과일바구니[100-99] // '수박'
+과일바구니[과일바구니[4]] // '참외'
+과일바구니[과일바구니[5]] // undefined`,
+  arrayExplain5: `const 과일바구니 = ['사과', '수박', '참외', '감', '잭푸르트', '용과']
+과일바구니.length // 6
+과일바구니[과일바구니.length - 1] // '용과'`,
+  forArrayQuestion1: `// 배열 내 모든 엘리먼트를 순서대로 console.log 해보세요
+const 과일바구니 = ['사과', '수박', '참외', '감', '잭푸르트', '용과']
+for (let i = ... )`,
+  forArrayAnswer1: `for (let i = 0; i < 과일바구니.length; i++) {
+    console.log(과일바구니[i])
+}`,
+  forArrayQuestion2: `// 배열 내 모든 엘리먼트를 역순으로 console.log 해보세요!
+const 투썸메뉴 = ['잉글리쉬 브랙퍼스트', '제주 유기농 녹차', '애플 민트티', '아메리카노', '흑임자 카페라떼', '복숭아 에이드']
+for (let i = ... )`,
+  forArrayAnswer2: `for (let i = 투썸메뉴.length - 1; i >= 0; i--) {
+  console.log(투썸메뉴[i])
+}`,
+  primitiveInReferenceQuestion: `const arr1 = [1, 2, 3]
+const arr2 = [arr1, 1, 2]
+
+console.log(arr1 === arr2[arr1[2] - arr1[1] - arr1[0]]) // ?`,
+
+  primitiveInReferenceAnswer: `const arr1 = [1, 2, 3]
+const arr2 = [arr1, 1, 2]
+
+console.log(arr1 === arr2[arr1[2] - arr1[1] - arr1[0]]) // true
+arr1[2] // 3
+arr1[1] // 2
+arr1[0] // 1
+// 3 - 2 - 1 = 0
+arr2[0] === arr1 // true`,
 };
 
 export default function Lecture() {
   return (
     <div className={styles.act_container}>
       <ActNavigator
-        curriculum="js"
+        curriculum='js'
         highlightActNum={1}
         chapterInfo={chaptersInfo.chapter3}
       ></ActNavigator>
@@ -134,10 +191,14 @@ export default function Lecture() {
         배열로 여행을 떠나기 전에 데이터 타입을 조금만 공부해보고
         넘어가겠습니다.
       </p>
-      <p>아래 코드는 true일까요? false일까요?</p>
-      <p>참입니다.</p>
-      <p>그렇다면 이 코드들은 true일까요? false일까요?</p>
-      <p>놀랍게도 이들은 false입니다.</p>
+      <p>우리는 아래 코드의 조건식이 true임을 알고 있습니다.</p>
+      <CodeBox code={lessonCode.primitiveCondition} />
+      <br />
+      <p>
+        그런데 이 코드들의 조건식은 왜 false일까요? 위의 코드와 별로
+        달라보이는게 없는데 말입니다.
+      </p>
+      <CodeBox code={lessonCode.referenceCondition} />
       <p>데이터가 똑같은데 왜 false인지 궁금하실 수 있는데요.</p>
       <p>이런 현상을 이해하기 위해서 원시 타입과 참조 타입을 알아야합니다.</p>
       <h3>원시 타입</h3>
@@ -148,18 +209,20 @@ export default function Lecture() {
       </p>
       <p>실제 값이 할당되고, 불변성을 지닌다는 것은 무슨 의미일까요?</p>
       <p>코드와 그림을 통해 살펴보겠습니다.</p>
+      <br />
       <CodeBox code={lessonCode.primitiveDeclarationA} />
       <p>변수 a가 선언되었습니다. a에는 숫자 25가 할당되어 있습니다.</p>
       <p>
         숫자 25가 메모리에 기록되고 식별자가 메모리 주소를 참조하는 것을
         대략적으로 그려보았습니다.
       </p>
+      <br />
       <CustomImageWithCaption
         imageSrc={primitive1}
         width={347}
         height={450}
-        alt="변수 a에 숫자 25가 할당"
-        caption="변수 a에 숫자형(원시타입) 데이터 25가 할당되었습니다."
+        alt='변수 a에 숫자 25가 할당'
+        caption='변수 a에 숫자형(원시타입) 데이터 25가 할당되었습니다.'
       />
       <p>
         숫자형 데이터 25는 메모리 특정 공간에 기록이 되고, 그 메모리 공간의
@@ -167,6 +230,7 @@ export default function Lecture() {
       </p>
       <br />
       <p>이 후 변수 b를 만들어보겠습니다. b에는 a가 할당됩니다.</p>
+      <br />
       <CodeBox code={lessonCode.primitiveDeclarationB} />
       <p>
         b가 a를 참조했으니, b는 a의 메모리 주소를 그대로 참조하고 있을까요??
@@ -177,19 +241,21 @@ export default function Lecture() {
         복제한 후, 별도의 메모리에 저장합니다.
       </p>
       <p>그림으로 묘사하면 아래와 같을 것입니다.</p>
+      <br />
       <CustomImageWithCaption
         imageSrc={primitive2}
         width={347}
         height={450}
-        alt="변수 B에 a가 할당"
-        caption="변수 b에 a가 할당되었지만, 25는 원시 타입이기 때문에 b는 a를 참조하지 않고 값만 복제해옵니다."
+        alt='변수 B에 a가 할당'
+        caption='변수 b에 a가 할당되었고, b는 a의 값을 복제해옵니다.'
       />
       <br />
       <p>이제 변수 a에 다른 값을 재할당 해보겠습니다.</p>
       <CodeBox code={lessonCode.primitiveRedeclarationA} />
+      <br />
       <p>a에 30을 재할당 했습니다.</p>
       <p>그렇다면 이전에 변수 a에 저장되어 있던 25는 어디로 가게 될까요?</p>
-      <p>정답은 25는 그대로 거기 있습니다.</p>
+      <p>25가 저장된 위치는 사실 변하지 않습니다.</p>
       <p>
         자바스크립트의 원시 타입은 불변성이기 때문에 메모리에 저장된 값이 바뀌지
         않습니다.
@@ -199,29 +265,24 @@ export default function Lecture() {
         메모리의 주소를 다시 참조합니다.
       </p>
       <p>그림으로 표시하면 아래와 같습니다.</p>
+      <br />
       <CustomImageWithCaption
         imageSrc={primitive3}
         width={347}
         height={450}
-        alt="변수 a에 30이 재할당"
-        caption="변수 a에 30을 재할당 했습니다. a는 30이 저장된 메모리 주소를 참조합니다."
+        alt='변수 a에 30이 재할당'
+        caption='변수 a에 30을 재할당 했습니다. a는 30이 저장된 메모리 주소를 참조합니다.'
       />
+      <br />
       <p>이제 변수 a는 30이 담긴 메모리의 주소를 참조합니다.</p>
+      <p>그렇다면 더이상 참조되지 않는 데이터인 25는 어떻게 될까요?</p>
       <p>
-        그렇다면 데이터가 있긴 하지만, 더이상 참조되지 메모리 주소는 어떻게
-        될까요?
-      </p>
-      <p>
-        어떤 변수에도 참조되지 않는 메모리는 <b>가비지 컬렉터</b>라는 것에 의해
-        회수될 것 입니다.
-      </p>
-      <p>
-        25는 더이상 참조되지 않습니다. 따라서 가비지 컬렉터에 의해 메모리가
+        어떤 변수에도 참조되지 않는 메모리는 <b>가비지 컬렉터</b>에 의해
         회수됩니다.
       </p>
       <p>
-        다만, 가비지 컬렉터는 언어 차원의 내부적 동작에 의해 실행되므로,
-        메모리의 회수 시기는 우리가 알 수 없습니다.
+        다만 가비지 컬렉터는 언어의 내부적 동작에 의해 실행되므로, 메모리의 회수
+        시기는 우리가 알 수 없습니다.
       </p>
       <br />
       <p>
@@ -244,31 +305,31 @@ export default function Lecture() {
         imageSrc={primitive4}
         width={347}
         height={450}
-        alt="변수 b에 a와 b를 더함"
-        caption="a + b는 표현식입니다. 표현식은 곧 값을 나타내지요. 새로운 값은 메모리의 다른 부분에 저장됩니다."
+        alt='변수 b에 a와 b를 더함'
+        caption='a + b는 표현식입니다. 표현식은 곧 값을 나타내지요. 새로운 값은 메모리의 다른 부분에 저장됩니다.'
       />
-      <p>
-        25 + 30 이라는 표현식이 계산되어 메모리의 다른 부분에 새롭게 쓰여집니다.
-      </p>
-      <p>그리고 변수 b는 표현식이 저장된 메모리의 주소를 참조하게 됩니다.</p>
+      <p>25 + 30 이라는 표현식이 계산된 후, 메모리에 저장됩니다.</p>
+      <p>그리고 변수 b는 새로운 값을 참조합니다.</p>
       <p>이로써</p>
       <ul>
         <li>
-          <b>원시 타입 데이터를 가진 변수에는 새로운 값이 할당된다는 것</b>
+          <b>
+            변수에 원시타입 데이터를 할당할때는 늘 새로운 값이 할당된다는 것과
+          </b>
         </li>
         <li>
-          <b>원시 타입 데이터는 불변성을 가짐</b>
+          <b>원시 타입 데이터는 불변성을 가진다는 것</b>
         </li>
       </ul>
       <p>을 설명할 수 있습니다.</p>
       <br />
-      <p>원시 타입의 선언을 실행 컨텍스트의 관점에서 한번 이해해볼까요?</p>
+      <h4>추가 파트 : 실행 컨텍스트와 원시타입 데이터</h4>
       <p>
-        변수 선언은 실행 컨텍스트와 연관이 깊다라는 것을 이전에 배웠었습니다.
+        원시 타입 데이터 변수 선언을 실행 컨텍스트의 관점에서 이해해보겠습니다.
       </p>
       <p>
         함수가 실행되면 실행 컨텍스트가 생성되고, 변수들이 환경 레코드에
-        등록되지요.
+        등록됩니다.
       </p>
       <p>
         실행 컨텍스트의 변수 등록을 메모리 관점에서 생각해보면 아래와 같을
@@ -278,15 +339,16 @@ export default function Lecture() {
         imageSrc={primitive5}
         width={643}
         height={355}
-        alt="실행 컨텍스트에 등록된 변수의 메모리 참조"
-        caption="실행 컨텍스트에 등록된 변수의 메모리 참조"
+        alt='실행 컨텍스트에 등록된 변수의 메모리 참조'
+        caption='실행 컨텍스트에 등록된 변수의 메모리 참조'
       />
       <p>위 그림은 원시 타입과 참조 타입을 이해하는데 필요합니다.</p>
       <p>이제 참조 타입에 대해서 학습해보도록 하겠습니다!</p>
       <h3>참조 타입</h3>
-      <p>먼저 코드로 퀴즈를 하나 내보겠습니다.</p>
+      <p>퀴즈입니다.</p>
       <p>두개의 console.log가 모두 출력될까요?</p>
       <CodeBox code={lessonCode.primitiveAndReference} />
+      <br />
       <p>
         <b>&ldquo;a와 b가 일치합니다!&ldquo;</b> 만 찍혀 나올 것입니다.
       </p>
@@ -313,65 +375,70 @@ export default function Lecture() {
         데이터를 담을 수 있다는 것을 표현하고자 product와 cart을 배열로
         만들었습니다.
       </p>
-      <p>이 두 배열은 메모리상에 어떻게 할당이 되고 있었을까요?</p>
+      <p>이 두 배열은 어떤 방식으로 메모리에 할당될까요?</p>
       <p>아래 그림을 보시겠습니다.</p>
       <CustomImageWithCaption
         imageSrc={reference1}
         width={840}
         height={400}
-        alt="참조타입이 메모리에 저장되는 그림"
-        caption="참조 타입은 힙(heap)이라는 메모리의 가변적인 부분을 활용합니다."
+        alt='참조타입이 메모리에 저장되는 그림'
+        caption='참조 타입은 힙(heap)이라는 메모리의 가변적인 부분을 활용합니다.'
       />
+      <p>참조타입은 값이 메모리 스택에 그대로 할당 되었던 원시 타입과 달리,</p>
       <p>
-        참조타입은 값이 메모리 스택에 그대로 할당이 되었던 원시 타입과 달리,
-      </p>
-      <p>
-        heap이라는 메모리의 가변적인 영역에 데이터가 할당되고, 식별자는 heap의
+        heap이라는 메모리의 가변적인 영역에 데이터가 할당되고, 변수는 heap의
         메모리 주소를 참조하게 됩니다.
       </p>
       <p>
-        즉, 식별자에 할당된 값이 다르므로, 둘을 비교 연산하면 false가 나오게
-        되는 것 입니다. ( 위 예시의 0x2345와 0x3516이 다르듯이요!)
+        즉, 변수에 할당된 값이 다르므로, 둘을 비교 연산하면 false가 나오게 되는
+        것 입니다. ( 위 예시의 0x2345와 0x3516이 다르듯이요!)
       </p>
       <br />
       <p>그렇다면 아래와 같은 상황은 어떨까요?</p>
       <CodeBox code={lessonCode.referenceArray2} />
-      <p>product 식별자에 배열이 할당되었고, cart는 product를 할당합니다.</p>
+      <br />
+      <p>product에 배열이 할당되었고, cart에는 product가 할당됩니다.</p>
       <p>
-        이 경우에는 product의 참조 주소값을 cart가 그대로 가져오므로, 두
-        식별자는 같은 배열을 참조하게 됩니다.
+        이 경우, product의 참조 주소값을 cart가 그대로 가져오므로 두 식별자는
+        같은 배열을 참조하게 됩니다.
       </p>
+      <br />
       <CustomImageWithCaption
         imageSrc={reference2}
         width={840}
         height={400}
-        alt="다른 식별자가 동일한 heap 주소를 참조하고 있는 그림"
-        caption="식별자 product와 cart는 동일한 heap 메모리 주소를 참조하게 됩니다."
+        alt='다른 식별자가 동일한 heap 주소를 참조하고 있는 그림'
+        caption='식별자 product와 cart는 동일한 heap 메모리 주소를 참조하게 됩니다.'
       />
       <p>약간은 감이 잡히셨을거라고 믿습니다.</p>
       <br />
-      <p>메모리의 관점에서 보면 아래 코드도 설명이 가능합니다.</p>
+      <p>따라서 메모리의 관점에서 보면 아래 코드를 설명할 수 있습니다.</p>
       <CodeBox code={lessonCode.referenceArray3} />
-      <p>cart는 초기에 생성된 product의 배열을 참조하고 있을 것 입니다.</p>
-      <p>그래서 첫번째 console.log는 true가 출력이 됩니다.</p>
+      <br />
+      <p>최초에 product에 배열이 할당 되었습니다.</p>
+      <p>cart는 product가 참조하고 있는 배열 주소를 복제해옵니다.</p>
       <p>
-        이 후 product에 <b>새로운 배열</b>을 할당했습니다.
-      </p>
-      <p>
-        즉, product는 heap 내부에 새롭게 생성된 배열의 주소를 참조하게 됩니다.
-      </p>
-      <p>
-        하지만 cart는 이전에 할당받은 배열의 주소를 그대로 참조하고 있기 때문에,
-        둘은 다른 배열이 됩니다.
+        즉, 둘은 같은 배열을 참조하고 있으므로 첫번째 비교에서는 true가
+        출력됩니다.
       </p>
       <br />
-      <p>이것이 원시 타입과 참조 타입의 차이점 입니다.</p>
+      <p>이후 product에 새로운 배열이 할당이 되었습니다.</p>
+      <p>이제 product는 다른 배열을 참조하게 됩니다.</p>
       <p>
-        원시와 참조의 구분에서 제일 중요한 것은 식별자가 무엇을 참조하는지 이며,
+        다만 cart는 여전히 이전에 생성된 배열 주소를 참조하고 있으므로, 둘을
+        비교하면 false가 출력됩니다.
+      </p>
+      <br />
+      <hr />
+      <br />
+      <p>원시 타입과 참조 타입의 차이점을 공부해 보았습니다.</p>
+      <p>내부적 원리까지 모두 한번에 이해하실 필요는 없습니다.</p>
+      <p>
+        다만, 개발 중 알 수 없는 현상이 일어났을 때, 그것을 캐치하고 해결하려면
+        언어의 내부 원리를 어느정도는 알고 있어야 합니다.
       </p>
       <p>
-        식별자가 값을 그대로 참조하는지, heap에 선언된 메모리 주소를
-        참조하는지가 관건입니다.
+        이제 원시 타입 데이터 종류와 참조 타입 데이터 종류를 알아보겠습니다.
       </p>
 
       <h3>원시 타입 데이터 종류</h3>
@@ -383,11 +450,96 @@ export default function Lecture() {
       <p>원시 타입을 제외한 모든 타입이 참조 타입이라고 볼 수 있습니다.</p>
 
       <h2>배열의 구성요소</h2>
-      <h3>엘리먼트</h3>
-      <h3>인덱스</h3>
+      <p>이제 본격적으로 배열에 대해 배워보겠습니다.</p>
+      <p>
+        앞서 말씀드렸듯이 배열은 특정 데이터들을 인덱스 순서대로 묶을 수 있는
+        기능을 하는 데이터 타입입니다.
+      </p>
+      <CodeBox code={lessonCode.arrayExplain1} />
+      <br />
+      <p>과일 이름을 표현한 문자열 데이터 4개를 배열에 추가한 그림입니다.</p>
+      <p>
+        배열 내부에 있는 각각의 데이터를 <b>엘리먼트</b>라고 하며, 배열 내
+        데이터에 접근할 수 있는 숫자를 <b>인덱스</b>라고 합니다.
+      </p>
+
+      <h3>1. 엘리먼트(element)</h3>
+      <CodeBox code={lessonCode.arrayExplain2} />
+      <br />
+      <p>배열 내 위치한 각각의 데이터를 엘리먼트(element)라고 부릅니다.</p>
+
+      <h3>2. 인덱스(index)</h3>
+      <CodeBox code={lessonCode.arrayExplain3} />
+      <br />
+      <p>
+        그리고 배열 내 엘리먼트에 바로 접근할 수 있는 숫자를 인덱스라고 합니다.
+      </p>
+      <p>인덱스는 0부터 시작해서 배열 길이 - 1으로 끝납니다.</p>
+      <p>우리의 과일바구니는 4개의 엘리먼트가 있었죠.</p>
+      <p>즉, 과일바구니의 배열 길이는 4이고, 배열의 최대 인덱스는 3입니다.</p>
+      <p>
+        배열에 인덱스로 접근해서 엘리먼트를 가져오는 문법은{" "}
+        <b>배열[인덱스숫자]</b> 입니다.
+      </p>
+      <p>
+        아래의 코드가 이해가 잘 가신다면 인덱스와 엘리먼트에 대한 기초적인
+        이해는 모두 되었다고 봐도 무방합니다.
+      </p>
+      <CodeBox code={lessonCode.arrayExplain4} />
+      <br />
+      <p>
+        위의 코드에서는 대괄호 내부에 숫자가 오고 있다는 것을 기억해주시기
+        바랍니다.
+      </p>
+      <p>
+        다만 인덱스를 벗어나는 숫자가 오게 될 경우, 배열 내 값이 없다는 의미로{" "}
+        <b>undefined</b>가 출력됩니다.
+      </p>
+
+      <h3>3. 길이(length)</h3>
+      <CodeBox code={lessonCode.arrayExplain5} />
+      <br />
+      <p>length는 배열의 프로퍼티(property)입니다.</p>
+      <p>
+        프로퍼티는 객체의 용어이고, 객체 챕터에서 더 자세하게 다룰 주제입니다.
+      </p>
+      <p>
+        자바스크립트에서 배열은 객체의 일부이고, 그렇기 때문에 별도의 프로퍼티를
+        가지고 있습니다.
+      </p>
+      <br />
+      <p>
+        length는 배열의 길이를 나타내는 숫자형 데이터로, 배열에 .length를 붙여
+        입력하면 배열의 길이를 알 수 있습니다.
+      </p>
+      <p>
+        배열의 최대 인덱스는 length - 1임을 꼭 기억해주세요! 굉장히 많이
+        사용됩니다!
+      </p>
       <h2>Exercise</h2>
-      <p>배열의 length, 배열의 최대 인덱스</p>
-      <p>배열 내 모든 데이터 console.log</p>
+      <ol>
+        <ExerciseCard
+          title='배열 내 모든 엘리먼트를 순서대로 console.log 해보세요!'
+          QuestionCode={lessonCode.forArrayQuestion1}
+          AnswerCode={lessonCode.forArrayAnswer1}
+        />
+        <ExerciseCard
+          title='배열 내 모든 엘리먼트를 역순으로 console.log 해보세요!'
+          QuestionCode={lessonCode.forArrayQuestion2}
+          AnswerCode={lessonCode.forArrayAnswer2}
+        />
+        <ExerciseCard
+          title='console.log 내부 조건식은 true일까요? false일까요?'
+          QuestionCode={lessonCode.primitiveInReferenceQuestion}
+          AnswerCode={lessonCode.primitiveInReferenceAnswer}
+        />
+      </ol>
+      <p>내용을 충분히 숙지하셨다면 다음 단계로 넘어가셔도 좋습니다.</p>
+      <p>
+        다음 장에서는 배열을 좀 더 재밌게 다룰 수 있게 해주는 배열 메서드와
+        프로토타입에 대해 공부해보겠습니다.
+      </p>
+      <p>고생하셨습니다!</p>
     </div>
   );
 }
