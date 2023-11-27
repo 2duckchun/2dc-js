ddfe9f0ddea375213e8271a2ab0ebe6b1ed7447d
2DC <kkts9308@gmail.com>
2023-11-27 16:40:24 +0900

    :art: 메인 화면에 메타톤 추가 ㅋㅋㅋ

--- src/app/(main)/(home)/page.tsx
+++ src/app/(main)/(home)/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Image from 'next/image';
+
+import Metaton from '/public/mettaton-ex-undertale.gif';
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div>메인화면</div>
+      <div>
+        <Image src={Metaton} width={498} height={493} alt='메타톤' />
+      </div>
     </main>
   );
 }
